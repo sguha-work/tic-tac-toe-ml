16863d5c4e30e00a40b705060fb78af06e728491
sguha-work <sguha1988.life@gmail.com>
2022-10-12 21:21:47 +0530

    working

--- tictactoe.py
+++ tictactoe.py
@@ -64,7 +64,7 @@ class TicTacToe:
 
     def __check_list_equality(self, list1, list2, length):
         flag = True
-        for index in enumerate(length):
+        for index in range(length):
             if list1[index] != list2[index]:
                 flag = False
                 break
@@ -108,7 +108,6 @@ class TicTacToe:
                 self.__brain_file_pointer = open(self.__brain_file_name, 'r')
                 data_in_brain = json.loads(self.__brain_file_pointer.read())
                 self.__brain_file_pointer.close()
-                computer_move = ''
                 if len(data_in_brain) == 0:
                     # no data exists in brain so selecting move randomly
                     computer_move = self.__get_random_unoccupied_coordinate()
@@ -119,15 +118,13 @@ class TicTacToe:
 
                     # Filtering out the user moves from occupied coordinates
                     # even position's data will be of user
-                    user_moves = range(0, len(self.__occupied_coordinates), 2)
-                    computer_moves = range(1, len(self.__occupied_coordinates), 2)
                     lost_game_moves = []
                     for data in data_in_brain:
                         check_list_result = self.__check_list_equality(data, self.__occupied_coordinates,
                                                                        len(self.__occupied_coordinates))
                         if check_list_result is True:
                             lost_game_moves.append(data)
-
+                    print(f'lost game moves {lost_game_moves}')
                     if len(lost_game_moves) != 0:
                         # need to apply intelligence before move
                         vulnerable_coordinates = []
